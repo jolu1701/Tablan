9b3002ec58301a7bf919180bad49ba5ecbf8a135
Jonatan Lundgren <jolu1701@student.miun.se>
2018-11-05 11:08:24 +0100

    Entityframe kört

---
+++ Tablån/Tablån/Models/Model1.cs
@@ -0,0 +1,48 @@
+﻿namespace Tablån.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Channels> Channels { get; set; }
+        public virtual DbSet<Genres> Genres { get; set; }
+        public virtual DbSet<Shows> Shows { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Channels>()
+                .Property(e => e.name)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Channels>()
+                .HasMany(e => e.Shows)
+                .WithOptional(e => e.Channels)
+                .HasForeignKey(e => e.Channel_id);
+
+            modelBuilder.Entity<Genres>()
+                .Property(e => e.name)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Genres>()
+                .HasMany(e => e.Shows)
+                .WithOptional(e => e.Genres)
+                .HasForeignKey(e => e.Genre_id);
+
+            modelBuilder.Entity<Shows>()
+                .Property(e => e.Title)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Shows>()
+                .Property(e => e.Info)
+                .IsFixedLength();
+        }
+    }
+}
